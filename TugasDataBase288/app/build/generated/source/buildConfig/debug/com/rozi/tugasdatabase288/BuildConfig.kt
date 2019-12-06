80606a8e3dd234825d167c8051abc335af62e244
muhammadrozi <44402186+muhammadrozi@users.noreply.github.com>
2019-12-06 12:08:45 +0700

    Add files via upload

---
+++ TugasDataBase288/app/build/generated/source/buildConfig/debug/com/rozi/tugasdatabase288/BuildConfig.kt
@@ -0,0 +1,12 @@
+package com.rozi.tugasdatabase288
+
+import java.lang.Boolean
+
+object BuildConfig {
+    val DEBUG = Boolean.parseBoolean("true")
+    const val APPLICATION_ID = "com.rozi.tugasdatabase288"
+    const val BUILD_TYPE = "debug"
+    const val FLAVOR = ""
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
---
+++ TugasDataBase288/app/src/main/java/com/rozi/tugasdatabase288/Fakultas.kt
@@ -0,0 +1,3 @@
+package com.rozi.tugasdatabase288
+
+data class Fakultas (val id_fakultas: String, val kode_fakultas:String?, val nama_fakultas:String?)
